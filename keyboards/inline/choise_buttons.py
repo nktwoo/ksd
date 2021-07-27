9b906b053962837fcbc2e1a2cdf81f153ffa2933
nktwoo <brnbkent2804@gmail.com>
2021-07-27 15:45:03 +0300

    'Inline Buttons'

---
+++ keyboards/inline/choise_buttons.py
@@ -0,0 +1,36 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from keyboards.inline.callback_datas import buy_callback
+
+
+choice = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Купить грушу', callback_data=buy_callback.new(
+                item_name='pear', quantity=1
+            )),
+            InlineKeyboardButton(text='Купить яблоки', callback_data=buy_callback.new(
+                item_name='apple', quantity=1
+            )),
+        ],
+        [
+            InlineKeyboardButton(text='Отмена', callback_data='cancel')
+        ]
+    ]
+)
+
+pear_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Купи тут', url='https://google.com')
+        ]
+    ]
+)
+
+apples_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Купи тут', url='https://google.com')
+        ]
+    ]
+)
